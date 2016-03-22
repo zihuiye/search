caa2a6e8a9810a6b48919153f185d116f8784999
zihuiye <zihuiye@gmail.com>
2016-03-22 03:55:39 -0400

    m

--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -8,9 +8,10 @@ class IndexAction extends Action {
 	$comand = "java -classpath '/home/ubuntu/myLucene:/usr/local/lucene-5.4.1/core/lucene-core-5.4.1.jar:/usr/local/lucene-5.4.1/analysis/common/lucene-analyzers-common-5.4.1.jar:/usr/local/lucene-5.4.1/queryparser/lucene-queryparser-5.4.1.jar' SearchFiles -index /home/ubuntu/myLucene/index -query ".$keyword." 2>&1"; 
 	//exec('export PATH=/home/ubuntu/myLucene/');
 	exec($comand,$res,$sta); 
+	$this->res=$res;
 	dump($res);
 	echo $sta;
-	//$this->display();
+	$this->display();
    }
     public function view(){
 	$filename = substr($_GET["filename"],4);
