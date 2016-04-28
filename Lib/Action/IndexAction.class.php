fb2ec196bada2a84523eae1737b9f6ce68379202
zihuiye <zihuiye@gmail.com>
2016-04-28 21:17:35 +0800

    com

--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -6,9 +6,9 @@ class IndexAction extends Action {
     public function search(){
 	$keyword = $_GET["keyword"];
 	$type = $_GET["type"];
-	$comand = "java -classpath '/home/ubuntu/myLucene:/usr/local/lucene-5.4.1/core/lucene-core-5.4.1.jar:/usr/local/lucene-5.4.1/analysis/common/lucene-analyzers-common-5.4.1.jar:/usr/local/lucene-5.4.1/queryparser/lucene-queryparser-5.4.1.jar' SearchFiles -index /home/ubuntu/myLucene/index -query ".escapeshellarg($keyword)." 2>&1"; 
-	$acomand = "java -classpath '/home/ubuntu/myLucene:/usr/local/lucene-5.4.1/core/lucene-core-5.4.1.jar:/usr/local/lucene-5.4.1/analysis/common/lucene-analyzers-common-5.4.1.jar:/usr/local/lucene-5.4.1/queryparser/lucene-queryparser-5.4.1.jar' SearchFiles -index /home/ubuntu/myLucene/AIndex -query ".escapeshellarg($keyword)." 2>&1"; 
-	$hcomand = "java -classpath '/home/ubuntu/myLucene:/usr/local/lucene-5.4.1/core/lucene-core-5.4.1.jar:/usr/local/lucene-5.4.1/analysis/common/lucene-analyzers-common-5.4.1.jar:/usr/local/lucene-5.4.1/queryparser/lucene-queryparser-5.4.1.jar' SearchFiles -index /home/ubuntu/myLucene/HIndex -query ".escapeshellarg($keyword)." 2>&1"; 
+	$command = "java -classpath '/home/ubuntu/myLucene:/usr/local/lucene-5.4.1/core/lucene-core-5.4.1.jar:/usr/local/lucene-5.4.1/analysis/common/lucene-analyzers-common-5.4.1.jar:/usr/local/lucene-5.4.1/queryparser/lucene-queryparser-5.4.1.jar' SearchFiles -index /home/ubuntu/myLucene/index -query ".escapeshellarg($keyword)." 2>&1"; 
+	$acommand = "java -classpath '/home/ubuntu/myLucene:/usr/local/lucene-5.4.1/core/lucene-core-5.4.1.jar:/usr/local/lucene-5.4.1/analysis/common/lucene-analyzers-common-5.4.1.jar:/usr/local/lucene-5.4.1/queryparser/lucene-queryparser-5.4.1.jar' SearchFiles -index /home/ubuntu/myLucene/AIndex -query ".escapeshellarg($keyword)." 2>&1"; 
+	$hcommand = "java -classpath '/home/ubuntu/myLucene:/usr/local/lucene-5.4.1/core/lucene-core-5.4.1.jar:/usr/local/lucene-5.4.1/analysis/common/lucene-analyzers-common-5.4.1.jar:/usr/local/lucene-5.4.1/queryparser/lucene-queryparser-5.4.1.jar' SearchFiles -index /home/ubuntu/myLucene/HIndex -query ".escapeshellarg($keyword)." 2>&1"; 
 	//exec('export PATH=/home/ubuntu/myLucene/');
 	
 	//dump($type);
@@ -18,31 +18,30 @@ class IndexAction extends Action {
 	}elseif($type=="WA"){
 		exec($acommand,$res,$sta);
 	}else{
-		exec($comand,$res,$sta); 
+		exec($command,$res,$sta); 
 	}
 	
 	
-	dump($res);
+	//dump($res);
 	//echo $sta;
 	$head = array_slice($res,0,2);
 	$result = array_slice($res,2);
 	$url=array();
 	$title=array();
-	dump($result);
-	for($i = 0;$i<10;$i++){
-		$line=explode(" ",$result[$i*9]);
-		array_push($url,$line[1]);
-		$line=$result[$i*9+1].$result[$i*9+2].$result[$i*9+3].$result[$i*9+4].$result[$i*9+5].$result[$i*9+6].$result[$i*9+7];
-		array_push($title,$line);
-		
+	foreach ($result as $key=>$line){
+		if ($key%2==0){
+			$line=explode(" ",$line);
+			array_push($url,$line[1]);
+		}else{
+			$line=explode(":",$line);
+			array_push($title,$line[1]);
+		}
 	}
-	dump($url);
-	dump($result);
 	for($i = 0;$i<10;$i++){
 		$out[$i]["url"]=$url[$i];
 		$out[$i]["title"]=$title[$i];
 	}
-	dump($out);
+	
 	if(count($result)==0){
 		$head[3] = "</br></br><h1>no result</h1>";
 	}
